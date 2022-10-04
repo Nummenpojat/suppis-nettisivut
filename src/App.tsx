89af7aa2b4f0f927e0ed7c440a1242475b2136a9
Aaro_laptop <aaro.heroja@gmail.com>
2022-10-04 15:54:38 +0300

    made simple prototype how fill module manager look like

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
+import Modules from "./components/Modules";
 
 function App() {
   return (
     <>
+      <Modules/>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        // added new 4 column grid as new4
+        'recipe3': 'repeat(auto-fit, minmax(300px, 500px))'
+      }
+    },
   },
   plugins: [],
 }
