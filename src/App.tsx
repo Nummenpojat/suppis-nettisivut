267a3da4342a3c481bd8c134ba6ab69dc07c568c
Aaro <aaro.heroja@gmail.com>
2022-11-20 18:37:32 +0200

    removed things that are not yet supported by API

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,14 @@ const MessageSender = () => {
   const [message, setMessage] = useState("")
   const [oneOrList, setOneOrList] = useState(true)
   const [all, setAll] = useState(false)
-  const [sudarit, setSudarit] = useState(false)
+
+  // TODO When API can handle specific age-groups add functionality to support them in frontend
+  /*const [sudarit, setSudarit] = useState(false)
   const [sheikit, setSheikit] = useState(false)
   const [tarpojat, setTarpojat] = useState(false)
   const [samoajat, setSamoajat] = useState(false)
   const [vaeltajat, setVaeltaja] = useState(false)
-  const [aikuiset, setAikuiset] = useState(false)
+  const [aikuiset, setAikuiset] = useState(false)*/
 
   const handleSubmit = (event: any) => {
     event.preventDefault()
@@ -23,29 +25,29 @@ const MessageSender = () => {
   const handleAllAgeGroups = () => {
     if (!all) {
       setAll(true)
-      setAikuiset(true)
+      /*setAikuiset(true)
       setSheikit(true)
       setSudarit(true)
       setTarpojat(true)
       setSamoajat(true)
-      setVaeltaja(true)
+      setVaeltaja(true)*/
       return;
     }
     setAll(false)
-    setAikuiset(false)
+    /*setAikuiset(false)
     setSheikit(false)
     setSudarit(false)
     setTarpojat(false)
     setSamoajat(false)
-    setVaeltaja(false)
+    setVaeltaja(false)*/
     return;
   }
 
   return (
     <div className="w-screen h-[calc(100vh-120px)]">
       <section className="h-full flex place-content-center">
-        <form className="m-5 flex flex-col w-1/4" onSubmit={handleSubmit}>
-          <h1 className="text-[35px]">Lähetä viesti</h1>
+        <form className="m-5 flex flex-col w-1/3 min-w-[300px]" onSubmit={handleSubmit}>
+          <h1 className="text-[35px] text-center">Lähetä viesti</h1>
           <label>Viesti:</label>
           <textarea rows={3}
                     onChange={(event) => {
@@ -54,7 +56,7 @@ const MessageSender = () => {
                     placeholder=" Type your message here..."
                     className="rounded-[5px] bg-gray-100"/>
           <label>
-            <input type="checkbox" className="m-3 checked:accent-mannynvihrea" checked={oneOrList}
+            <input type="checkbox" className="mx-1.5 my-3 checked:accent-mannynvihrea" checked={oneOrList}
                    onClick={() => setOneOrList(!oneOrList)}/>
             Lähetä viesti monille ihmisille
           </label>
@@ -62,16 +64,18 @@ const MessageSender = () => {
             oneOrList ?
               <div>
                 <label>
-                  Mille ikäkausille lähetetään viesti
+                  Mille ikäkausille viesti lähetetään
                 </label><br/>
                 <ul>
                   <AgeGroupCheckbox checked={all} onClick={handleAllAgeGroups} agegroup="Kaikki ikäkaudet"/>
-                  <AgeGroupCheckbox checked={sudarit} onClick={() => setSudarit(!sudarit)} agegroup="Sudenpennut"/>
-                  <AgeGroupCheckbox checked={sheikit} onClick={() => setSheikit(!sheikit)} agegroup="Seikkailijat"/>
-                  <AgeGroupCheckbox checked={tarpojat} onClick={() => setTarpojat(!tarpojat)} agegroup="Tarpojat"/>
-                  <AgeGroupCheckbox checked={samoajat} onClick={() => setSamoajat(!samoajat)} agegroup="Samoajat"/>
-                  <AgeGroupCheckbox checked={vaeltajat} onClick={() => setVaeltaja(!vaeltajat)} agegroup="Vaeltajat"/>
-                  <AgeGroupCheckbox checked={aikuiset} onClick={() => setAikuiset(!aikuiset)} agegroup="Aikuiset"/>
+                  {/* Api can't yet handle specific age-groups
+                    <AgeGroupCheckbox checked={sudarit} onClick={() => setSudarit(!sudarit)} agegroup="Sudenpennut"/>
+                    <AgeGroupCheckbox checked={sheikit} onClick={() => setSheikit(!sheikit)} agegroup="Seikkailijat"/>
+                    <AgeGroupCheckbox checked={tarpojat} onClick={() => setTarpojat(!tarpojat)} agegroup="Tarpojat"/>
+                    <AgeGroupCheckbox checked={samoajat} onClick={() => setSamoajat(!samoajat)} agegroup="Samoajat"/>
+                    <AgeGroupCheckbox checked={vaeltajat} onClick={() => setVaeltaja(!vaeltajat)} agegroup="Vaeltajat"/>
+                    <AgeGroupCheckbox checked={aikuiset} onClick={() => setAikuiset(!aikuiset)} agegroup="Aikuiset"/>
+                  */}
                 </ul>
               </div> :
               <div>
