46dbeff80249f2aeb67ffe1869ac4eccc50b1b0b
Aaro <aaro.heroja@gmail.com>
2022-11-23 18:03:16 +0200

    navbar now scales same way as on the main page

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,14 @@ function App() {
   return (
     <>
       <Navbar>
+        <NavElement text="TAPAHTUMAT" linksTo="/events"/>
         <NavElement text="WHATSAPP" linksTo="/whatsapp"/>
         <NavElement text="PÄÄSIVULLE" linksTo="https://nummenpojat.fi"/>
       </Navbar>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<></>}/>
+          <Route path="/events" element={<></>}/>
           <Route path="/whatsapp" element={<MessageWrapper/>}/>
         </Routes>
       </BrowserRouter>
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,21 +1,28 @@
 import React from "react";
 import '../../theme/nummarit.css'
 
-export default function Navbar({ children }: any) {
-  return <header>
-    <div className="h-[120px] bg-partionsininen py-[20px] px-[40px] flex">
-      <a href="/" className="mr-auto">
-        <img
-          src="https://nummenpojat.kuvat.fi/kuvat/Kuvituskuvaa%20ja%20grafiikkaa/Graafinen%20ohje%20ja%20br%C3%A4ndimateriaalit/Lippukunnan%20logot/HN%20ovaali%20logo.png?img=img4k"
-          className="h-[80px] w-[160px]"/>
-      </a>
-      <nav>
-        <ul className="h-full flex place-items-center">
-          {
-            children
-          }
-        </ul>
-      </nav>
-    </div>
-  </header>;
+export default function Navbar({children}: any) {
+  return (
+    <header>
+      <div className="h-[120px] bg-partionsininen py-[20px] px-[40px] flex ">
+        <a href="/" className="mr-auto">
+          <img
+            src="https://nummenpojat.kuvat.fi/kuvat/Kuvituskuvaa%20ja%20grafiikkaa/Graafinen%20ohje%20ja%20br%C3%A4ndimateriaalit/Lippukunnan%20logot/HN%20ovaali%20logo.png?img=img4k"
+            className="h-[80px] w-[160px]"/>
+        </a>
+        <nav className="hidden md:flex">
+          <ul className="h-full flex place-items-center">
+            {
+              children
+            }
+          </ul>
+        </nav>
+        <nav className="md:hidden place-items-center flex">
+          <button>
+            <img width="30px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/Octicons-three-bars.svg/1200px-Octicons-three-bars.svg.png"/>
+          </button>
+        </nav>
+      </div>
+    </header>
+  );
 }
