fc995840357545a283d78da5df76f84b71f6b509
Aaro <aaro.heroja@gmail.com>
2022-11-25 18:44:31 +0200

    made page where you can generate whatsapp session

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,25 @@ import Navbar from "./components/navbar/navbar";
 import NavElement from "./components/navbar/navElement";
 import './theme/nummarit.css'
 import './theme/webteema.css'
-import MessageWrapper from "./components/messages/messageWrapper";
+import MessageWrapper from "./components/whatsapp/messages/messageWrapper";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+import QrWrapper from "./components/whatsapp/qrWrapper";
 
 function App() {
   return (
     <>
       <Navbar>
         <NavElement text="TAPAHTUMAT" linksTo="/events"/>
-        <NavElement text="WHATSAPP" linksTo="/whatsapp"/>
+        <NavElement text="LUO WHATSAPP ISTUNTO" linksTo="/whatsapp/new"/>
+        <NavElement text="LÄHETÄ VIESTI" linksTo="/whatsapp/send"/>
         <NavElement text="PÄÄSIVULLE" linksTo="https://nummenpojat.fi"/>
       </Navbar>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<></>}/>
           <Route path="/events" element={<></>}/>
-          <Route path="/whatsapp" element={<MessageWrapper/>}/>
+          <Route path="/whatsapp/new" element={<QrWrapper/>}/>
+          <Route path="/whatsapp/send" element={<MessageWrapper/>}/>
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/components/whatsapp/qrWrapper.tsx
@@ -0,0 +1,35 @@
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+
+const QrWrapper = () => {
+
+  const [qr, setQr] = useState("")
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/modules/whatsapp/new")
+      .then((response) => {
+
+        // Regex replaces all + with %2B because + can't be used in urls parameter values, so they are converted to %2B which is equivalent
+        setQr(response.data.replace(/\+/g, "%2B"))
+
+      })
+      .catch((reason) => {
+        throw `Error: "${reason}" -- Application was not able to reach the server`
+      })
+  }, [])
+
+  return (
+    <>
+      <div className="w-screen h-[calc(100vh-120px)] place-items-center flex flex-col">
+        <h1 className="text-[35px]">GENERATING NEW QR CODE</h1>
+        {qr == "" ?
+          <p>Loading...</p>
+          :
+          <img src={`https://api.qrserver.com/v1/create-qr-code/?size=500x500&data=${qr}`}
+               className="m-5 h-full aspect-square"/>
+        }
+      </div>
+    </>
+  )
+}
+export default QrWrapper
