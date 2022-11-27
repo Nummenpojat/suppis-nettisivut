54daf66a330a87348cccbcf3a9e41c794d8d5900
Aaro <aaro.heroja@gmail.com>
2022-11-27 12:47:36 +0200

    button template that stars an event

--- src/components/events/eventListElement.tsx
+++ src/components/events/eventListElement.tsx
@@ -3,12 +3,11 @@ import React from "react";
 const EventListElement = (props: { event: any }) => {
   return (
     <div className="border-b-2 m-2">
-      <h1 className="text-varvunvihrea text-[20px]">
-        {props.event.name}
-      </h1>
-      <p className="text-[15px] mx-1">
-        {props.event.occasionType}
-      </p>
+      <div className="flex ">
+        <h1 className="text-varvunvihrea text-[20px] mr-auto">{props.event.name}</h1>
+        <button className="text-[20px] font-passionOne text-mannynvihrea">+</button>
+      </div>
+      <p className="text-[15px] mx-1">{props.event.occasionType}</p>
       <p className="text-[15px]  mx-1">
         { `${props.event.startDate.substring(0, props.event.startDate.indexOf('T'))} - ${props.event.endDate.substring(0, props.event.endDate.indexOf('T'))}` }
       </p>
