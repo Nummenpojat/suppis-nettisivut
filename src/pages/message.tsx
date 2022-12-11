e8c01824541acd9b83e30fdf13b4176109d25a39
Aaro <aaro.heroja@gmail.com>
2022-12-11 17:10:13 +0200

    now client is able to handle incoming qr codes

--- src/pages/message.tsx
+++ src/pages/message.tsx
@@ -1,19 +1,23 @@
 import React, {useState} from "react";
 import {getIdTokenForApiCall} from "../firebaseConfig";
-import axios from "axios";
+import axios, {AxiosResponse} from "axios";
 
 export default function MessageWrapper() {
   const [message, setMessage] = useState("")
   const [phoneNumber, setPhoneNumber] = useState("")
+  const [qr, setQr] = useState("")
   const [toList, setToList] = useState(true)
   const [all, setAll] = useState(true)
+  const [showQr, setShowQr] = useState(false)
 
   const handleSend = async (event: any) => {
     event.preventDefault()
     if (!toList) {
       try {
+
+        //
         const idToken = await getIdTokenForApiCall()
-        const result = await axios.post("http://localhost:3001/whatsapp/send/one", {
+        const result: AxiosResponse = await axios.post("http://localhost:3001/whatsapp/send/one", {
           number: phoneNumber,
           message: message
         }, {headers: {idtoken: idToken}})
@@ -22,56 +26,79 @@ export default function MessageWrapper() {
 
       } catch (error: any) {
 
-        if (error.data.type == "qr") {
-          console.warn(error.data.message)
-        }
+        if (error.response.status == 409) {
 
-        console.error(error.message)
+          // Regex replaces all + with %2B because + can't be used in urls parameter values, so they are converted to %2B which is equivalent
+          setQr(error.response.data.replace(/\+/g, "%2B"))
+
+          setShowQr(true)
+          return
+        } else {
+          alert(error.response.data)
+        }
       }
     }
   }
 
   return (
     <div className="w-screen h-[calc(100vh-120px)]">
-      <section className="h-full flex place-content-center">
-        <form className="m-5 flex flex-col w-1/3 min-w-[300px]">
-          <h1 className="text-[35px] text-center">Lähetä viesti</h1>
-          <label>Viesti:</label>
-          <textarea rows={3} onChange={(event) => setMessage(event.target.value)}
-                    placeholder=" Kirjoita viesti tähän . . . " className="rounded-[5px] bg-gray-100 p-0.5"/>
-          <label>
-            <input type="checkbox" className="mx-1.5 my-3 checked:accent-mannynvihrea" defaultChecked={toList}
-                   onClick={() => setToList(!toList)}/>
-            Lähetä viesti monelle ihmisille
-          </label>
-          {
-            toList ?
-              <>
-                <label>
-                  Mille ikäkausille viesti lähetetään
-                </label><br/>
-                <ul>
-                  <li>
-                    <input type="checkbox" className="m-1.5 checked:accent-mannynvihrea" defaultChecked={all}
-                           onClick={() => setAll(!all)}/>
-                    <label>Kaikki ikäkaudet</label>
-                  </li>
-                </ul>
-              </>
-              :
-              <>
-                <label>
-                  Numero mihin viesti lähetetään
-                </label><br/>
-                <input onChange={(event) => setPhoneNumber(event.target.value)}
-                       className="bg-gray-100 w-full rounded-[5px] p-0.5" type="tel" placeholder=" Esim +3581234567"
-                       autoComplete="on"/>
-              </>
+      {showQr ?
+        <section className="place-items-center flex flex-col">
+          <h1 className="text-[35px]">GENERATING NEW QR CODE</h1>
+          {qr == "" ?
+            <p>Loading...</p>
+            :
+            <img src={`https://api.qrserver.com/v1/create-qr-code/?size=500x500&data=${qr}`}
+                 className="m-5 max-h-[400px] aspect-square" alt="qr code"/>
           }
-          <input onClick={handleSend} type="submit" value="LÄHETÄ VIESTI"
-                 className="my-5 bg-mannynvihrea p-2 rounded-[5px] font-passionOne text-white text-xl"/>
-        </form>
-      </section>
+          <button
+            className="my-5 bg-mannynvihrea p-2 rounded-[5px] font-passionOne text-white text-xl text-center w-1/4 min-w-[300px]"
+            onClick={() => setShowQr(false)}>
+            SULJE
+          </button>
+        </section>
+        :
+        <section className="h-full flex place-content-center">
+          <form className="m-5 flex flex-col w-1/3 min-w-[300px]">
+            <h1 className="text-[35px] text-center">Lähetä viesti</h1>
+            <label>Viesti:</label>
+            <textarea rows={3} onChange={(event) => setMessage(event.target.value)}
+                      placeholder=" Kirjoita viesti tähän . . . " className="rounded-[5px] bg-gray-100 p-0.5"/>
+            <label>
+              <input type="checkbox" className="mx-1.5 my-3 checked:accent-mannynvihrea" defaultChecked={toList}
+                     onClick={() => setToList(!toList)}/>
+              Lähetä viesti monelle ihmisille
+            </label>
+            {
+              toList ?
+                <>
+                  <label>
+                    Mille ikäkausille viesti lähetetään
+                  </label><br/>
+                  <ul>
+                    <li>
+                      <input type="checkbox" className="m-1.5 checked:accent-mannynvihrea" defaultChecked={all}
+                             onClick={() => setAll(!all)}/>
+                      <label>Kaikki ikäkaudet</label>
+                    </li>
+                  </ul>
+                </>
+                :
+                <>
+                  <label>
+                    Numero mihin viesti lähetetään
+                  </label><br/>
+                  <input onChange={(event) => setPhoneNumber(event.target.value)}
+                         className="bg-gray-100 w-full rounded-[5px] p-0.5" type="tel" placeholder=" Esim +3581234567"
+                         autoComplete="on"/>
+                </>
+            }
+            <input onClick={handleSend} type="submit" value="LÄHETÄ VIESTI"
+                   className="my-5 bg-mannynvihrea p-2 rounded-[5px] font-passionOne text-white text-xl"/>
+          </form>
+        </section>
+      }
+
     </div>
   )
 }
